f6df3026beea4c133673d7e2d83856c2cfe4c646
glever <glen.vermeylen@gmail.com>
2019-04-22 13:47:12 +0200

    wire up the router state with custom serializer - app/store/reducers: create custom serializer from RouterStateSnapshot to RouterStateUrl which gets saved in the store - app module: import (= use feature) StoreRouterConnectingModule - add our custom serializer to the DI providers list ==> in redux dev tools you now see the ROUTER_NAVIGATION action with state payload of our custom RouterStateUrl

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,6 +1,10 @@
 import * as fromRouter from "@ngrx/router-store";
 import { ActionReducerMap, createFeatureSelector } from "@ngrx/store";
-import { Params } from "@angular/router";
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Params
+} from "@angular/router";
 
 export interface RouterStateUrl {
   url: string;
@@ -19,3 +23,17 @@ export const reducers: ActionReducerMap<State> = {
 export const getRouterState = createFeatureSelector<
   fromRouter.RouterReducerState<RouterStateUrl>
 >("routerReducer");
+
+export class CustomSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    const { url } = routerState; // == const url = routerState.url;
+    const { queryParams } = routerState.root;
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+    const { params } = state;
+    return { url, queryParams, params };
+  }
+}
